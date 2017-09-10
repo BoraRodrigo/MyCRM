6418eaa8282436a67e4b6fa38bb33c107098d11a
Rodrigo <Rodrigo@RODRIGO-PC>
2017-09-10 20:22:37 -0300

    At..

--- src/br/edu/facear/servelet/CadastrarClienteServelet.java
+++ src/br/edu/facear/servelet/CadastrarClienteServelet.java
@@ -1,6 +1,5 @@
 package br.edu.facear.servelet;
 
-import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
